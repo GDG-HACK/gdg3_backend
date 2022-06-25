0a1362391089b7118dcc9d0880ba82c94de6952c
kim-yeji <2214yjgm@gmail.com>
2022-06-25 19:17:56 +0900

    DOCS: 기본 Spring 폴더 생성

---
+++ src/main/java/com/example/gdg3/Gdg3Application.java
@@ -0,0 +1,13 @@
+package com.example.gdg3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdg3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gdg3Application.class, args);
+	}
+
+}
