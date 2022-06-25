97c515d37e6946366d10d0cc64db4ec851dc28ba
Lee Eunji <lej970703@naver.com>
2022-06-25 23:01:34 +0900

    CHORE: gradle 추가

---
+++ src/main/java/search/SearchController.java
@@ -0,0 +1,14 @@
+package search;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class SearchController {
+    @GetMapping("stores")
+    public @ResponseBody
+
+}
