37b904af7bc2db0328813dd5f5031f159602a07a
kim-yeji <2214yjgm@gmail.com>
2022-06-26 06:35:24 +0900

    CHORE: git 캐시 삭제

--- src/main/java/com/example/gdg3/search/SearchService.java
+++
@@ -1,6 +0,0 @@
-package com.example.gdg3.search;
-
-public interface SearchService {
-    // public search(String location, int pageNum, int size);
-
-}
--- src/main/java/com/example/gdg3/search/SearchServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.gdg3.search;
-
-
-public class SearchServiceImpl implements SearchService {
-//    private final SearchRepository searchRepository;
-//
-//    public
-}
